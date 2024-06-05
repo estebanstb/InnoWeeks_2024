bfbbde648013eedc39f431c8da2532d2aa6899ae
TiaSous <tiago.rodrigues2@eduvaud.ch>
2024-06-05 10:59:57 +0200

    get formation in json

--- innoWeek-SiteWeb/components/listFormation.vue
+++ innoWeek-SiteWeb/components/listFormation.vue
@@ -1,31 +1,14 @@
-<script setup lang="ts">
-    const items = [
-    {
-        id: 1,
-        name: "vue",
-        image:"https://picsum.photos/600/800?random=1",
-        description:"test"
-    },
-    {
-        id: 2,
-        name: "react",
-        image:"https://picsum.photos/600/800?random=2",
-        description:"test"
-    },
-    {
-        id: 3,
-        name: "rip",
-        image:"https://picsum.photos/600/800?random=3",
-        description:"test"
-    }
-]
+<script setup>
+    import formation from '~/data/formation.json'
+    const items = ref(formation.formations)
+    console.log(items.value)
 </script>
 
 <template>
     <div class="flex p-12">
         <div v-for="item in items" :key="item.id">
-            <img :src="item.image" class="w-12" alt="">
-            <p>{{ item.name }}</p>
+            <img src="" class="w-12" alt="">
+            <p>{{ item.title }}</p>
             <p>{{ item.description }}</p>
         </div>
     </div>
