e06b10941fc770f0b111f49557d5dcabdc8e0c33
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-06-07 14:34:37 +0200

    create formations view

--- innoWeek-SiteWeb-2/components/appHeader.vue
+++ innoWeek-SiteWeb-2/components/appHeader.vue
@@ -3,11 +3,15 @@
         <p class="font-bold text-4xl flex items-center">ITFORM</p>
         <ul class="flex items-center">
             <li>
-            <NuxtLink to="/">
-                Accueil
-            </NuxtLink> 
+                <NuxtLink to="/">
+                    Accueil
+                </NuxtLink>
             </li>
-            <li>Formations</li>
+            <li>
+                <NuxtLink to="/formations">
+                    Formations
+                </NuxtLink>
+            </li>
             <li>
                 <NuxtLink to="/search">
                     Recherche personnaliser
@@ -23,8 +27,8 @@
 </template>
 
 <style scoped>
-ul > li{
-font-weight: lighter;
-margin-left: 50px;
+ul>li {
+    font-weight: lighter;
+    margin-left: 50px;
 }
 </style>
---
+++ innoWeek-SiteWeb-2/pages/formations.vue
@@ -0,0 +1,11 @@
+<script setup> //setup pour faire du javascript basique
+// Code javascript
+</script>
+<template>
+     <div id="">
+        <!-- Code HTML -->
+     </div>
+</template>
+<style scoped> /* scoped défini que le style css n'a d'effet que sur ce fichier */
+/* Code CSS */
+</style>
