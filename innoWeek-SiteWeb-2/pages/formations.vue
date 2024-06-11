ddd8727521779d5443803e5c41321e871086ee6a
Esteban <esteban.lebet@gmail.com>
2024-06-11 15:32:24 +0200

    feat(listFormation): first version of "Listes de Formation"

--- innoWeek-SiteWeb-2/pages/formations.vue
+++ innoWeek-SiteWeb-2/pages/formations.vue
@@ -1,11 +1,34 @@
-<script setup> //setup pour faire du javascript basique
-// Code javascript
+<script setup>
+import ListFormation from '~/components/listFormation.vue'
+import formation from '~/data/formation.json'
+
+const list = ref([])
+
+onMounted(() => {
+  formation.formations.forEach((element) => {
+    if (element.id > 0) {
+      list.value.push(element)
+    }
+  })
+})
+
 </script>
 <template>
      <div id="">
-        <!-- Code HTML -->
+        <h1>Nos propositions de formations</h1>
+        <ListFormation v-if="list.length" :formation="list" />
      </div>
 </template>
 <style scoped> /* scoped défini que le style css n'a d'effet que sur ce fichier */
-/* Code CSS */
+   h1 {
+      font-size: 40px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+
+      border: 8px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 100px;
+   }
 </style>
